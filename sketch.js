24b629b45d993b9f91daf0cf55473513f2b6c6ee
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-06-04 16:06:25 +0530

    -Added 2nd branch line

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@ function setup() {
 
 function draw() {
   background(51);
-  translate(200,height); // Setting up 0,0 co-ord. to middle-bottom of screen.
+  translate(200,height);  // Setting up (0,0 co-ord.) init point to middle-bottom of screen.
   var len = 100;
-  stroke(255);          // Line Color set to white(255).
+  stroke(255);            // Line Color set to white(255).
   branch(100);          
 }
 
 function branch(len) {
   line(0,0,0,-len);
+  translate(0,-len);      // Moving init point to new drawn line's top.
+  rotate(PI/4);           // Rotate 45deg. the page.
+  line(0,0,0,-len);       // Drawing new brach.
 }
