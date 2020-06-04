6e26c65110e5b6cf2c160913f16306e9e2d7bae4
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-06-04 15:55:39 +0530

    -Added branch function

--- sketch.js
+++ sketch.js
@@ -4,5 +4,12 @@ function setup() {
 
 function draw() {
   background(51);
+  translate(200,height);
+  var len = 100;
+  stroke(255);
+  branch(100);
+}
 
+function branch(len) {
+  line(0,0,0,-len);
 }
