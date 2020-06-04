d7193ef762be0d35fe91401138e8f6f367923979
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-06-04 16:53:00 +0530

    -Direction change of branches

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
 var len = 100;
-var angle = PI / 4; // Calculating 45deg. 
+var angle = 0; // Calculating 45deg. 
 
 var slider;
 
 function setup() {
   createCanvas(400,400);
-  slider = createSlider(0, TWO_PI, PI / 4);  // Creating slider to choose values between 0 and 45deg.
+  slider = createSlider(0, TWO_PI, PI / 4, 0.01);  // Creating slider to choose values between 0 and 45deg. and step-size 0.01 .
 }
 
 function draw() {
@@ -19,10 +19,11 @@ function draw() {
 function branch(len) {
   line(0,0,0,-len);
   translate(0,-len);      // Moving init point to new drawn line's top.
-  rotate(angle);           // Rotation by angle.
-
   if( len > 4){           // Recursion Base only draw when length is greater than 4 pix.
-    branch(len * 0.67);     // Recursive call
+    rotate(angle);        // Rotation in right side (+angle).
+    branch(len * 0.67);   // Recursive call
+    rotate(-angle);       // Rotation in left side (-angle).
+    branch(len * 0.67);               
   }
     //line(0,0,0,-len * 0.67);       // Drawing new brach and reducing it by 2/3 size.
 }
