43a51924fcf0fe80b7f2e2c45b99fe249843dbbb
Animesh Kahara <animeshcyberphysco@gmail.com>
2020-06-04 15:58:12 +0530

    -Addin translate to funtion.

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ function setup() {
 
 function draw() {
   background(51);
-  translate(200,height);
+  translate(200,height); // Setting up 0,0 co-ord. to middle-bottom of screen.
   var len = 100;
-  stroke(255);
-  branch(100);
+  stroke(255);          // Line Color set to white(255).
+  branch(100);          
 }
 
 function branch(len) {
